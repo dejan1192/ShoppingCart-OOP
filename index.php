86c10c6f9a2a2815d27602b27112077797661781
dejan1192 <dejan1192@gmail.com>
2020-09-20 15:05:53 +0200

    index fix

--- index.php
+++ index.php
@@ -10,13 +10,15 @@ $product = new Item(1,"Iphone", 41, 6);
 $product2 = new Item(2, "Samsung Galaxy", 55, 10 );
 $product3 = new Item(3,"Huawei Y6", 35, 10);
 $product4 = new Item(4,"Honor 10", 50, 10);
+$product5 = new Item(5, "Notebook D112", 300, 5);
 
 //Dodavanje proizvoda
-$cartItem = $cart->addProduct($product); // objekat klase CartItem
-$cartItem->increaseQty(3); // true ukoliko je kolicina u opsegu dozvoljene kolicine, false ukoluiko nije
-$cart->addProduct($product2, 4); // objekat klase Cart item, moguce proslediti kolicinu kao dodatni parametar
+$cartItem = $cart->addProduct($product); // Instanca klase CartItem
+$cartItem->increaseQty(3); // True ukoliko je kolicina u opsegu dozvoljene kolicine, false ukoluiko nije
+$cart->addProduct($product2, 4); // Instanca klase Cart item, moguce proslediti kolicinu kao dodatni parametar
 $cart->addProduct($product3);
 $cart->addProduct($product4, 2);
+$cart->addProduct($product5);
 
 //Smanjenje kolicine proizvoda
 $cartItem->decreaseQty(3);
@@ -37,11 +39,11 @@ echo "Total price: ".$cart->getTotal() ."$".PHP_EOL.PHP_EOL;
 
 
 // Placanje sa  Visa , Mastercard, Paypal
-$customer = new Customer("John Smith", new PayPal());
+$customer = new Customer("John Smith", new Mastercard());
 
 $customer->checkout($cart); //Preuzima odabrane proizvode
 $customer->getInvoice(); //Izdaje racun
-$paymentType = $customer->getPaymentType(); // Objekat  klase Mastercard,Visa ili PayPal
+$paymentType = $customer->getPaymentType(); // Instanca PaymentInterface-a
 $customer->payOrder($paymentType); // Placanje izabranom metodom..
 
 
